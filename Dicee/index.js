b34f52f2fb7ce8459503b2bbacde2630dbea5ce7
Bruno Dias <135241168+brunodias8@users.noreply.github.com>
2023-09-26 09:32:04 -0400

    Add files via upload

---
+++ Dicee/index.js
@@ -0,0 +1,12 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+var image1 = document.querySelector(".img1");
+var image2 = document.querySelector(".img2");
+
+function dice1(){
+    image1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    image2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+}
+
+dice1();
